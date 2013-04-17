2950f7edd908aadf89506dcc3af8063808264ca3
seoin <seoin@snuev.com>
2013-04-17 19:57:56 +0900

    seoin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Profile::Application.config.secret_token = 'f2215f349a9ddd4676d24612fe12a78e3c0499f1cdad971f86ee48a7e0e17fd71d0a22eeedd83a32460a3c3bbda831965414eecaba72c1c1486a2d47d5218765'
